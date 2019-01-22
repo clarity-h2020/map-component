4d0ed16a71177cd881cfa1375a4ac0a4850d0153
Jan Peters-Anders <jan.peters-anders@ait.ac.at>
2019-01-22 11:27:12 +0100

    Remove test commits.

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-  const test = document.createElement('jan');
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
