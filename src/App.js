8b6f314f562d2b787859fbbd1c89818821dd06cf
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-08-23 16:01:07 +0200

    #37 revert parts of the routing havoc

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { Route, Switch, BrowserRouter} from "react-router-dom";
-import { createBrowserHistory } from 'history';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import GenericMap from "./components/GenericMap";
 import CharacteriseHazardMap from "./components/CharacteriseHazardMap";
 import ExposureMap from "./components/ExposureMap";
@@ -10,23 +9,18 @@ import StudyArea from "./components/StudyArea";
 import VulnerabilityMap from "./components/VulnerabilityMap";
 //import ReactLoading from "react-loading";
 
-export const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL
-});
-
 /**
  * @class
  */
 export default class App extends React.Component {
 
   render() {
-    console.log(history.location.pathname);
     return (
         <main>
           <BrowserRouter>
             <Switch>
-              <Route exact path={`${history.location.pathname}`} component={GenericMap} />
-              <Route exact path={`${history.location.pathname}/CharacteriseHazardMap`} component={CharacteriseHazardMap} />
+              <Route exact path="/" component={GenericMap} />
+              <Route exact path="/CharacteriseHazardMap/" component={CharacteriseHazardMap} />
               <Route exact path="/ExposureMap/" component={ExposureMap} />
               <Route exact path="/HazardLocalEffectsMap/" component={HazardLocalEffectsMap} />
               <Route exact path="/StudyArea/" component={StudyArea} />
